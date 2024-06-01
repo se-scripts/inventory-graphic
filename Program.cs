23f3324b7a6d6984b9db1f7409ae4a730505225d
li-guohao <git@liguohao.cn>
2024-06-01 23:00:56 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,181 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        /*
+        * R e a d m e
+        * -----------
+        * Graphical inventory display for LCD.
+        * 
+        * @see <https://github.com/malware-dev/MDK-SE/wiki>
+        * @see <https://github.com/li-guohao/se-scritps>
+        * @see <https://www.bilibili.com/read/cv27778300/>
+        * @author [Hi.James](https://space.bilibili.com/368005035)
+        * @author [li-guohao](https://space.bilibili.com/3493285257546598)
+        */
+
+        MyIni _ini = new MyIni();
+
+        List<IMyCargoContainer> cargoContainers = new List<IMyCargoContainer>();
+        List<IMyTextPanel> panels = new List<IMyTextPanel>();
+        List<IMyTextPanel> panels_Items_All = new List<IMyTextPanel>();
+        List<IMyTextPanel> panels_Items_Ore = new List<IMyTextPanel>();
+        List<IMyTextPanel> panels_Items_Ingot = new List<IMyTextPanel>();
+        List<IMyTextPanel> panels_Items_Component = new List<IMyTextPanel>();
+        List<IMyTextPanel> panels_Items_AmmoMagazine = new List<IMyTextPanel>();
+        List<IMyTextPanel> panels_Overall = new List<IMyTextPanel>();
+        List<IMyBatteryBlock> batteryList = new List<IMyBatteryBlock>();
+        List<string> spritesList = new List<string>();
+
+
+        Dictionary<string, string> translator = new Dictionary<string, string>();
+
+        const int itemAmountInEachScreen = 35, facilityAmountInEachScreen = 20;
+        const float itemBox_ColumnInterval_Float = 73, itemBox_RowInterval_Float = 102, amountBox_Height_Float = 24, facilityBox_RowInterval_Float = 25.5f;
+        const string information_Section = "Information";
+        const string translateList_Section = "Translate_List", length_Key = "Length";
+        int counter_ProgramRefresh = 0, counter_ShowItems = 0, counter_Panel = 0;
+        double counter_Logo = 0;
+
+        Color background_Color = new Color(0, 35, 45);
+        Color border_Color = new Color(0, 130, 255);
+
+        public struct ItemList
+        {
+            public string Name;
+            public double Amount;
+        }
+        ItemList[] itemList_All;
+        ItemList[] itemList_Ore;
+        ItemList[] itemList_Ingot;
+        ItemList[] itemList_Component;
+        ItemList[] itemList_AmmoMagazine;
+
+
+        public struct ComparisonTable
+        {
+            public string Name;
+            public string BluePrintName;
+            public double Amount;
+            public bool HasItem;
+        }
+
+
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Once | UpdateFrequency.Update10;
+
+            SetDefultConfiguration();
+
+            BuildTranslateDic();
+
+        }
+
+        public void SetDefultConfiguration()
+        {
+            // This time we _must_ check for failure since the user may have written invalid ini.
+            MyIniParseResult result;
+            if (!_ini.TryParse(Me.CustomData, out result))
+                throw new Exception(result.ToString());
+
+            //  Initialize CustomData
+            string dataTemp;
+            dataTemp = Me.CustomData;
+            if (dataTemp == "" || dataTemp == null)
+            {
+                _ini.Set(information_Section, "LCD_Overall_Display", "LCD_Overall_Display | Fill In CustomName of Panel");
+                _ini.Set(information_Section, "LCD_Inventory_Display", "LCD_Inventory_Display:X | X=1,2,3... | Fill In CustomName of Panel");
+                _ini.Set(information_Section, "LCD_Ore_Inventory_Display", "LCD_Ore_Inventory_Display:X | X=1,2,3... | Fill In CustomName of Panel");
+                _ini.Set(information_Section, "LCD_Ingot_Inventory_Display", "LCD_Ingot_Inventory_Display:X | X=1,2,3... | Fill In CustomName of Panel");
+                _ini.Set(information_Section, "LCD_Component_Inventory_Display", "LCD_Component_Inventory_Display:X | X=1,2,3... | Fill In CustomName of Panel");
+                _ini.Set(information_Section, "LCD_AmmoMagazine_Inventory_Display", "LCD_AmmoMagazine_Inventory_Display:X | X=1,2,3... | Fill In CustomName of Panel");
+                _ini.Set(translateList_Section, length_Key, "1");
+                _ini.Set(translateList_Section, "1", "AH_BoreSight:More");
+
+                Me.CustomData = _ini.ToString();
+            }// End if
+
+            GridTerminalSystem.GetBlocksOfType(cargoContainers, b => b.IsSameConstructAs(Me));
+            GridTerminalSystem.GetBlocksOfType(panels, b => b.IsSameConstructAs(Me));
+            GridTerminalSystem.GetBlocksOfType(panels_Overall, b => b.IsSameConstructAs(Me) && b.CustomName.Contains("LCD_Overall_Display"));
+            GridTerminalSystem.GetBlocksOfType(panels_Items_All, b => b.IsSameConstructAs(Me) && b.CustomName.Contains("LCD_Inventory_Display:"));
+            GridTerminalSystem.GetBlocksOfType(panels_Items_Ore, b => b.IsSameConstructAs(Me) && b.CustomName.Contains("LCD_Ore_Inventory_Display:"));
+            GridTerminalSystem.GetBlocksOfType(panels_Items_Ingot, b => b.IsSameConstructAs(Me) && b.CustomName.Contains("LCD_Ingot_Inventory_Display:"));
+            GridTerminalSystem.GetBlocksOfType(panels_Items_Component, b => b.IsSameConstructAs(Me) && b.CustomName.Contains("LCD_Component_Inventory_Display:"));
+            GridTerminalSystem.GetBlocksOfType(panels_Items_AmmoMagazine, b => b.IsSameConstructAs(Me) && b.CustomName.Contains("LCD_AmmoMagazine_Inventory_Display:"));
+            GridTerminalSystem.GetBlocksOfType(batteryList, b => b.IsSameConstructAs(Me));
+
+
+            //  incase no screen
+            if (panels.Count < 1)
+            {
+                if (Me.SurfaceCount > 0)
+                {
+                    Me.GetSurface(0).GetSprites(spritesList);
+                }
+            }
+            else
+            {
+                panels[0].GetSprites(spritesList);
+            }
+
+
+        }
+
+
+        public void GetConfiguration_from_CustomData(string section, string key, out string value)
+        {
+
+            // This time we _must_ check for failure since the user may have written invalid ini.
+            MyIniParseResult result;
+            if (!_ini.TryParse(Me.CustomData, out result))
+                throw new Exception(result.ToString());
+
+            string DefaultValue = "";
+
+            // Read the integer value. If it does not exist, return the default for this value.
+            value = _ini.Get(section, key).ToString(DefaultValue);
+        }
+
+        public void BuildTranslateDic()
+        {
+            string value;
+            GetConfiguration_from_CustomData(translateList_Section, length_Key, out value);
+            int length = Convert.ToInt16(value);
+
+            for (int i = 1; i <= length; i++)
+            {
+                GetConfiguration_from_CustomData(translateList_Section, i.ToString(), out value);
+                string[] result = value.Split(':');
+
+                translator.Add(result[0], result[1]);
+            }
+        }
+
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+
+        }
+    }
+}
